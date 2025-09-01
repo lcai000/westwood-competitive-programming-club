3de3d67c263bfb9bda8e02aa9ab3657845eedeb4
lcai000 <landoncai777@gmail.com>
2025-09-01 10:47:47 -0500

    bash for folder creation

---
+++ createProblemFolder.sh
@@ -0,0 +1,32 @@
+createProblemFolder(){
+    BASE_DIR="Problems_Solutions"
+    cd $BASE_DIR
+    read -p "Enter problem name: " problem_name
+
+    mkdir $problem_name
+    cd $problem_name
+    touch problem.txt
+
+    mkdir solutions
+    cd solutions
+    touch robot.txt
+
+    mkdir ideal_solutions
+    mkdir student_solutions
+
+    cd ideal_solutions
+    touch solution.cpp
+    touch solution.java
+    touch solution.py
+
+    cd ..
+
+    cd student_solutions
+    touch robot.txt
+
+    cd ..
+    cd ..
+    cd ..
+    echo "problem folder $problem_name created"
+}
+createProblemFolder
