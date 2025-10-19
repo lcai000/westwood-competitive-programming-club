f58538e43fabe833ee4779c7d0abbf3a6052572e
lcai000 <landoncai777@gmail.com>
2025-10-18 19:14:24 -0500

    add java solution for 231A Team

---
+++ Problems_Solutions/Team/solutions/ideal_solutions/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int count = 0;
+        for (int i = 0; i < num; i++){
+            int currSum = 0;
+            for (int j = 0; j < 3; j++){
+                currSum+=scanner.nextInt();
+            }
+            if (currSum>=2){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
